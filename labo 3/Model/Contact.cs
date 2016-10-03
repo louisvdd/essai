535e28e357e976061c4faedb1cfabeb7d24cd18a
louisvdd <etu28841@student.henallux.be>
2016-10-03 10:23:30 +0200

    2h

---
+++ labo 3/Model/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+    }
+}
---
+++ labo 3/labo 3/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Model;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Labo3
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public  partial class MainPage : Page
+    {
+        private List<Contact> lstContacts = new List<Contact>()
+        {
+            new Contact("Charlier", "Isabelle", "isabelle.charlier@henallux.be"),
+            new Contact("Scholtes", "Samuel", "samuel.scholtes@henallux.be")
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = lstContacts;
+        }
+    }
+}
